47679f480cc9a1ed8b75af10c23a5c5fc4247ace
vadymcy <vadymmargari@gmail.com>
2021-02-28 14:04:31 +0200

    Customizing Generated HTML files

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,11 @@ module.exports = {
                 path.join(process.cwd(), 'build/**/*')
             ]
         }),
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hello World',
+            meta: {
+                description: 'Some description'
+            }
+        }),
     ]
 }
